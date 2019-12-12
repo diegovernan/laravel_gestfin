0f70b053149df75e9447a1ca4dcea5061cbf4769
Diego Vernan <diegovernan@gmail.com>
2019-12-12 09:14:55 -0300

    Reestruturaçao das rotas e remoçao do welcome

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::post('/home/category', 'HomeController@storeCategory')->name('home.store.category');
 Route::post('/home/transaction', 'HomeController@storeTransaction')->name('home.store.transaction');
