a35a816b0b245e6d3279af34b9b762a38cd042d5
Diego Vernan <diegovernan@gmail.com>
2019-12-20 09:27:58 -0300

    Implementaçao da alteraçao de senha na profile view

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -54,14 +54,16 @@
             </script>
             @endif
 
+            <!-- Profile name card -->
             <div class="card">
-                <div class="card-header text-center d-flex justify-content-between align-items-center">
+                <div class="card-header text-center">
                     <span>Meu perfil</span>
-                    <a href="{{ route('home') }}" class="btn btn-sm btn-secondary">Voltar</a>
                 </div>
 
                 <div class="card-body">
-                    <form method="post" action="{{ route('profile.update', $user->id) }}">@method('PATCH')@csrf
+                    <form method="post" action="{{ route('profile.update', $user->id) }}">
+                        @csrf
+                        @method('PATCH')
                         <div class="form-group">
                             <label for="Input1">Nome</label>
                             <input type="text" class="form-control" id="Input1" name="name" value="{{ old('name') ?? $user->name }}">
@@ -70,6 +72,44 @@
                         <button type="submit" class="btn btn-primary">Salvar</button>
                     </form>
                 </div>
+            </div>
+
+            <br>
+
+            <!-- Profile password card -->
+            <div class="card">
+                <div class="card-header text-center">
+                    <span>Segurança</span>
+                </div>
+
+                <div class="card-body">
+                    <form method="post" action="#">
+                        @csrf
+                        @method('PATCH')
+                        <div class="form-group">
+                            <label for="Input2">Senha atual</label>
+                            <input type="text" class="form-control" id="Input2" name="old_password">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="Input3">Nova senha</label>
+                            <input type="text" class="form-control" id="Input3" name="new_password">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="Input4">Confirmação da nova senha</label>
+                            <input type="text" class="form-control" id="Input4" name="password_confirm">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Salvar</button>
+                    </form>
+                </div>
+            </div>
+
+            <br>
+
+            <div class="float-right">
+                <a href="{{ route('home') }}" class="btn btn-secondary">Voltar</a>
             </div>
         </div>
     </div>
