5a027aa36d28c16373d5427a4e2b3df35dd4f7b7
Diego Vernan <diegovernan@gmail.com>
2019-12-22 18:19:04 -0300

    Implementaçao da traduçao no confirm

---
+++ resources/lang/pt-BR/confirm.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'Confirm Password' => 'Confirmar senha',
+    'Please confirm your password before continuing.' => 'Por favor, confirme sua senha antes de continuar.',
+    'Password' => 'Senha',
+    'Forgot Your Password?' => 'Esqueceu sua senha?',
+
+];
