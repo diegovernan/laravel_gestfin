1cd8aff91e412dc94e4d20b8f6b6932c31de9bc8
Diego Vernan <diegovernan@gmail.com>
2019-12-22 17:35:15 -0300

    Implementaçao da traduçao no verify

---
+++ resources/lang/pt-BR/verify.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'Verify Your Email Address' => 'Verifique o seu e-mail',
+    'A fresh verification link has been sent to your email address.' => 'Um novo link de verificação foi enviado para o seu e-mail.',
+    'Before proceeding, please check your email for a verification link.' => 'Antes de prosseguir, por favor procure pelo link de verificação no seu e-mail.',
+    'If you did not receive the email' => 'Se você não recebeu o e-mail',
+    'click here to request another' => 'clique aqui para solicitar outro',
+
+];
