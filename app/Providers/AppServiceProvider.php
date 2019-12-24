b5a54d228c5bf12c0240f23656325404b82b709b
Diego Vernan <diegovernan@gmail.com>
2019-12-24 20:34:16 -0300

    Implementaçao no reset de senha e acertos na traduçao das notificaçoes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Notifications\VerifyEmail;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,10 +26,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Override the email notification for verifying email
+        // Comentar tudo para voltar para o inglês
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
             $mail = new MailMessage;
-            $mail->subject('Bem-vindo!');
-            $mail->markdown('emails.verify', ['url' => $url]);
+            $mail->subject('Confirmação de e-mail!');
+            $mail->line('Por favor, clique no botão abaixo para confirmar o seu e-mail.');
+            $mail->action('Confirmar e-mail', $url);
+            $mail->line('Caso você não tenha feito esta solicitação, basta ignorar este e-mail.');
+            return $mail;
+        });
+
+        // Override the email notification for verifying email
+        // Comentar tudo para voltar para o inglês
+        ResetPassword::toMailUsing(function ($notifiable, $token) {
+            $mail = new MailMessage;
+            $mail->subject('Redefinição de senha!');
+            $mail->line('Você está recebendo este e-mail porque nós recebemos uma solicitação de redefinição de senha da sua conta.');
+            $mail->action('Resetar senha', url(config('app.url') . route('password.reset', ['token' => $token, 'email' => $notifiable->getEmailForPasswordReset()], false)));
+            $mail->line('Esse link de redefinição de senha expirará em 60 minutos.');
+            $mail->line('Caso você não tenha feito esta solicitação, basta ignorar este e-mail.');
             return $mail;
         });
     }
